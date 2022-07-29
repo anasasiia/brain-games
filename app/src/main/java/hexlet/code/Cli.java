0db8c3fcda93e030568ccc1d69c5d86bc6b79625
anasasiia <sheldoncooperbuga@gmail.com>
2022-07-29 20:49:09 +0400

    add method getName

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,2 +1,13 @@
-package hexlet.code;public class Cli {
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void getName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("May I have your name?");
+
+        var name = sc.nextLine();
+        System.out.println("Hello, " + name);
+    }
 }
